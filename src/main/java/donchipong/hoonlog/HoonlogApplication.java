66e025795f7e1fef0ba1999141cb9aa16ca4dfeb
ParangBird <seekerman@naver.com>
2024-01-10 22:07:35 +0900

    first commit

---
+++ src/main/java/donchipong/hoonlog/HoonlogApplication.java
@@ -0,0 +1,13 @@
+package donchipong.hoonlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoonlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoonlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/donchipong/hoonlog/HoonlogApplicationTests.java
@@ -0,0 +1,13 @@
+package donchipong.hoonlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoonlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
